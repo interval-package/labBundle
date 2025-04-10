88fb0b959f755116776f10d675a26e5a614854cd
zaterval <ziang_zheng@foxmail.com>
2025-04-10 07:38:36 +0000

    add extension linking

---
+++ scripts/setup/link_extension.sh
@@ -0,0 +1,4 @@
+ln -s /workspace/isaaclab/_lab_bundle/factoryIsaac      /workspace/isaaclab/source/factoryIsaac
+ln -s /workspace/isaaclab/_lab_bundle/algs/RoboWorld    /workspace/isaaclab/source/RoboWorld
+ln -s /workspace/isaaclab/_lab_bundle/algs/rsl_rl       /workspace/isaaclab/source/rsl_rl
+# ln -s /workspace/isaaclab/_lab_bundle/tasks/trackerLab  /workspace/isaaclab/source/
